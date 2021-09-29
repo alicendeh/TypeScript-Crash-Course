16de1b53b27867f9c21b0d8521701cb8f8c303c6
alicendeh <alicendeh16@gmail.com>
2021-09-29 09:04:58 +0100

    learnt the basic types in type script

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-window.alert("hey");
+//objects
+// const fet: {
+//   name: string;
+//   street: number;
+//   junction: string;
+//   good: boolean;
+// } = {
+//   name: "FET",
+//   street: 8,
+// };
+// console.log(fet.name);
+//Arrays
+// const person = {
+//   name: "Alice",
+//   hobbies: [2, 4],
+//   affin: ["yaya", 1, "alice", 2, false], //tupples
+// };
+// let trial;
+// trial = person.affin.push(9);
+// console.log(person.affin);
+//ENUMS
+var Pos;
+(function (Pos) {
+    Pos["FRONTEND"] = "Yaya";
+    Pos["BACKEND"] = "Alice";
+    Pos[Pos["FULLSTACK"] = 0] = "FULLSTACK";
+    Pos[Pos["DEVOPS"] = 1] = "DEVOPS";
+})(Pos || (Pos = {}));
+var devSet = {
+    role: Pos.BACKEND
+};
+console.log(devSet.role);
--- index.ts
+++ index.ts
@@ -1,1 +1,37 @@
-window.alert("hey");
+//objects
+
+// const fet: {
+//   name: string;
+//   street: number;
+//   junction: string;
+//   good: boolean;
+// } = {
+//   name: "FET",
+//   street: 8,
+// };
+// console.log(fet.name);
+
+//Arrays
+// const person = {
+//   name: "Alice",
+//   hobbies: [2, 4],
+//   affin: ["yaya", 1, "alice", 2, false], //tupples
+// };
+
+// let trial;
+// trial = person.affin.push(9);
+
+// console.log(person.affin);
+
+//ENUMS
+enum Pos {
+  FRONTEND = "Yaya",
+  BACKEND = "Alice",
+  FULLSTACK = 0,
+  DEVOPS,
+}
+let devSet = {
+  role: Pos.BACKEND,
+};
+
+console.log(devSet.role);
